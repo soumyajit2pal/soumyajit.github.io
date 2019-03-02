765ca832edb1d043b391a0657e2b6c095e8fe1c0
soumyajit2pal <soumyajit2pal@gmail.com>
2019-03-02 19:18:54 +0530

    4th commit

--- js/alerm.js
+++ js/alerm.js
@@ -4,7 +4,6 @@ function showTime(){
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
     var session = "AM";
-    
     if(h == 0){
         h = 12;
     }
@@ -16,11 +15,9 @@ function showTime(){
         h = h - 12;
         session = "PM";
     }
-    
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
-    
     var time = h + ":" + m + ":" + s + " " + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
